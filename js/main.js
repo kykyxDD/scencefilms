366ef9ce2cac68d351aee139603b879c2404f98b
sergey.lemeshenko <grey@volcanoideas.com>
2015-09-24 14:23:58 +0300

    Main menu overhaul.

---
+++ js/main.js
@@ -0,0 +1,78 @@
+var app = angular.module('app', [])
+.controller('appController', ['$scope', '$http', '$location', '$document', function($s, $http, $location, $doc){
+
+    console.log("controller", $doc)
+    
+    var transition = new Transition($doc[0].querySelector('.transition'))
+    var main_menu = new Anim_menu($doc[0].querySelector('#main_menu'))
+    
+    main_menu.onClick = function(page) {
+        transition.open()
+        change_page(page)
+    }
+    
+    $s.$on('$locationChangeSuccess', function(event){
+        console.log('blah')
+    })
+
+    $s.onItemClick = function() {
+        console.log("data")        
+    }
+    
+    $s.onMenuHeaderClick = function() {
+        main_menu.hide_header()
+        main_menu.expand()
+        transition.expand()
+    }
+    
+    $s.onMenuCloseClick = function() {
+        main_menu.collapse()
+        transition.collapse()  
+    }
+
+    $http.get("data.json", {})
+    .success(angular.bind(null, function(data, status){
+        console.log("loaded", transition.show())
+        $s.data = data
+        
+        main_menu.init(data.pages)
+        main_menu.show_header(0.3)
+    }))
+    
+}])
+.directive('mainmenu', function(){
+    
+    console.log("main_menu")
+    
+    return {
+        
+        scope: {
+            pages: '=ngModel'        
+        },
+        template: '<li ng-repeat="page in pages" class="" ng-class="{over: isOver == $index}" ng-mouseover="onOver($index)" ng-mouseout="onOut()">' +
+                        '<div class="itm{{$index}}"> {{page.name}} </div>' + 
+                        '<div class="cont_line">' + 
+                            '<div class="line_menu"></div>' +
+                        '</div>' +
+                    '</li>',
+        link: function(scope, element, attr) {
+            scope.isOver = -1;
+            scope.onOver = function(index) {
+                scope.isOver = index
+            }
+            scope.onOut = function() {
+                scope.isOver = -1
+            }
+        }
+    } 
+})
+.directive('transition', function() {
+    return {
+        scope: {
+            ref: "=ref"
+        },
+        link: function(scope, element, attr) {
+            scope.ref = new Transition(element[0])            
+        }
+    }
+})
--- js/script.js
+++ js/script.js
@@ -1,46 +1,139 @@
-function Anim_menu (argument) {
+function Anim_menu (cont) {
 
 	var self = this;
-	this.bg = document.querySelector('#bg');
-	
-	this.load = document.querySelector('#load');
-	this.div_num_load = this.load.querySelector('.num_load');
-	this.title_page = document.querySelector('#title_page');
-	var style_title_page = ScreenObject.decorate_element.call(this.title_page);
-
-	this.cont_menu = document.querySelector('#cont_menu');
-	this.title_menu = this.cont_menu.querySelector('.title_menu');
-	var title_menu_style = ScreenObject.decorate_element.call(this.title_menu);
-	this.list_menu = this.cont_menu.querySelector('.list_menu');
-	this.arr_list_items = this.list_menu.querySelectorAll('li');
-	this.close_menu = this.list_menu.querySelector('.close_menu');
-	this.addclass(this.cont_menu, 'vis_title');
-	this.transition = new Transition(document.querySelector(".transition"));
-	this.centerScreen();
-
-	this.event_itm_menu();
-
-	this.transition.show();
-
-	for (var i = 0; i < this.arr_list_items.length; i++){
-		var elem = this.arr_list_items[i];
-		ScreenObject.decorate_element.call(elem)
-	}
-	
+    this.cont = cont
+    
+	this.header_menu = this.cont.querySelector('.header_cont');
+    this.header_label = this.cont.querySelector('.haeder_label')
+    this.header_cont = this.cont.querySelector('.header_cont')
+    this.lines = cont.querySelectorAll('.line')
+    
+    this.menu_cont = this.cont.querySelector('.menu_cont')
+	this.close_btn = this.cont.querySelector('.close_btn')
+   
+    ScreenObject.decorate_element.apply(this.close_btn)
+    ScreenObject.decorate_element.apply(this.header_label)
+    ScreenObject.decorate_element.apply(this.header_cont)
+    
+    for (var i=0; i<this.lines.length; i++) {
+        ScreenObject.decorate_element.apply(this.lines[i])
+    }
+    
+    this.items = []
+    
+    /*
 	this.title_menu.addEventListener('click', function(){
-		self.hiddenTitleMenu();
-		self.transition.expand();
+		self.hiddenTitleMenu()
+		self.transition.expand()
 	});
 	this.close_menu.addEventListener('click', function(){
-		self.hiddenListMenu();
-		self.transition.collapse();
+		self.hiddenListMenu()
+		self.transition.collapse()
 		
 	})
 	window.addEventListener('resize', function(){
-		self.centerScreen();
+		self.centerScreen()
 	})
+    */
 }
 Anim_menu.prototype = {
+    
+    init: function(pages) {
+        
+        for (var i=0; i<pages.length; i++) {
+            var page = pages[i]
+            var itm = document.createElement("div")
+            itm.className = "cont_list_menu"
+            itm.innerHTML = "<div class='label'>" + page.name + "</div><div class='line'></div>"
+            itm.line = itm.querySelector('.line')
+            ScreenObject.decorate_element.apply(itm)
+            ScreenObject.decorate_element.apply(itm.line)
+            
+            itm.y = 55 * i
+            
+            this.menu_cont.appendChild(itm)
+            this.items.push(itm)
+        }
+        
+        /*
+        for (var i = 0; i < this.arr_list_items.length; i++){
+            var elem = this.arr_list_items[i]
+            ScreenObject.decorate_element.call(elem)
+        }
+        */
+        
+        this.reset()
+    },
+    
+    reset: function() {
+        
+        console.log('reset')
+        
+        for (var i=0; i<this.lines.length; i++) {
+            var line = this.lines[i]
+            line.x = -30
+            line.visible = false
+        }
+        
+        for (var i=0; i<this.items.length; i++) {
+            var itm = this.items[i]
+            itm.visible = false
+            itm.x = -155
+        }
+        
+        this.close_btn.y = -90
+        this.close_btn.x = 0
+        this.header_label.y = -66
+        this.header_label.x = 0
+        this.header_cont.x = 0
+        this.header_cont.y = -6
+        this.header_cont.alpha = 1
+        
+    },
+    
+    show_header: function(show_delay) {
+
+        var show_delay = show_delay || 0
+    
+        TweenLite.to(this.header_label, 0.42, {y: 0, delay: show_delay})
+        
+        var delay = show_delay + 0.0
+        var l = this.lines.length
+    
+        for (var i=0; i<l; i++) {
+            var line = this.lines[i]
+            TweenLite.to(line, 0.17, {x: 0, delay: delay, ease: Power0.easeIn, onStart: function() {this.visible = true}, onStartScope: line})
+            delay += 0.1*(l-(i+1))
+        }
+        
+        TweenLite.to(this.header_cont, 0.3, {y: 0, delay: show_delay+0.42})
+    },
+    
+    hide_header: function() {
+        TweenLite.to(this.header_cont, 0.5, {x: 150, alpha: 0})
+    },
+    
+    collapse: function() {
+        
+    },
+    
+    expand: function(expand_delay) {
+        
+        var expand_delay = expand_delay || 0
+        var delay = 0
+        
+        for (var i=0; i<this.items.length; i++) {
+            var itm = this.items[i]
+            itm.visible = true
+            itm.alpha = 0
+            delay = expand_delay + i*0.1
+            TweenLite.to(itm, 0.3, {x: 0, alpha: 1, delay: delay, ease: Power0.easeOut})
+        }
+        
+        TweenLite.to(this.close_btn, 0.2, {y: 0, delay: expand_delay + 0.3})
+        TweenLite.to(this.close_btn, 0.9, {y: 7, delay: expand_delay + 0.3 + 0.2})
+    },
+    
 	centerScreen: function(){
 		var windowWidth = document.documentElement.clientWidth;
 		var windowHeight = document.documentElement.clientHeight;
@@ -109,15 +202,12 @@ Anim_menu.prototype = {
 			},600)
 		})
 
-
 		elem.addEventListener('click', function(e){	
 			self.title_page.className = '';
 			// self.title_page.y = elem.y+20;
 			var target = e.target || e.srcElement;
 			var text = target.textContent.toLowerCase();
-			console.log(text)
 
-			
 			self.title_page.textContent = text;
 			self.loadPages(text);
 
