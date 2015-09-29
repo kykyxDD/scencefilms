97e13f76b8f3215f2bf4409edaaef54622d2a1fc
NastyaShcherbakova <kykyxDD@gmail.com>
2015-09-29 12:26:04 +0300

    update

--- js/squary.js
+++ js/squary.js
@@ -68,6 +68,9 @@ Squares.prototype = {
         page.elem.text_rhom = text;
     },
 
+    resize: function(){
+
+    },
 
     pos_rhom: function(page){
     	var scape_text = this.scape_text;
@@ -120,15 +123,32 @@ Squares.prototype = {
         };
     },
     resize: function(){
-    	var left = this.rights;
-    	var clLeft = document.body.clientWidth
-    	var percent = left/clLeft;
-    	if(percent > 0.8){
-    		this.cont_rhom_right.style.left = '20%';
-    		this.cont_rhom_right.style.right = '';
-    	} else {
-    		this.cont_rhom_right.style.left = '';
-    		this.cont_rhom_right.style.right = this.rights + 'px';
-    	}
+    	// var left = this.rights;
+    	// var clLeft = document.body.clientWidth
+    	// var percent = left/clLeft;
+        if(!this.sq_arr_right || !this.sq_arr_left) return
+
+        this.sq_width = Math.round(document.body.clientWidth/6);
+        this.rights = Math.round((this.sq_width*this.scape_text*3)*0.95);
+
+        this.cont_rhom_right.style.right = this.rights + 'px';
+        this.cont_rhom_left.style.left = Math.round((-this.sq_width)*0.4) + 'px';
+        this.cont_rhom_left.style.bottom = Math.round((-this.sq_width)*0.4) + 'px';
+
+        // console.log(this.sq_arr_right,this.sq_arr_left)
+
+        for(var k = 0; k < this.sq_arr_right.length; k++){
+            this.pos_rhom(this.sq_arr_right[k]);
+        }
+        for(var k = 0; k < this.sq_arr_left.length; k++){
+            this.pos_rhom(this.sq_arr_left[k]);
+        }
+    	// if(percent > 0.8){
+    	// 	this.cont_rhom_right.style.left = '20%';
+    	// 	this.cont_rhom_right.style.right = '';
+    	// } else {
+    	// 	this.cont_rhom_right.style.left = '';
+    	// 	this.cont_rhom_right.style.right = this.rights + 'px';
+    	// }
     },
 }
